a688b92e525244415da78c889d4f0bd49d27c992
anton <57831993+antonrotar@users.noreply.github.com>
2026-02-20 00:13:34 +0100

    Simplify names

--- model/test/batch_test.py
+++ model/test/batch_test.py
@@ -9,9 +9,9 @@ def make_batch_and_order_line(
     batch_sku: str = "TEST-SKU",
     line_sku: str = "TEST-SKU",
 ) -> tuple[Batch, OrderLine]:
-    batch = Batch(reference="batch-001", sku=batch_sku, quantity=batch_quantity)
+    batch = Batch(reference="batch-1", sku=batch_sku, quantity=batch_quantity)
     order_line = OrderLine(
-        order_reference="order-001", sku=line_sku, quantity=line_quantity
+        order_reference="order-1", sku=line_sku, quantity=line_quantity
     )
 
     return batch, order_line
@@ -42,10 +42,10 @@ def test_allocate_order_line_to_batch_with_mismatched_sku_raises_exception():
 
 
 def test_allocate_different_order_lines_to_batch_reduces_available_quantity():
-    batch = Batch(reference="batch-001", sku="TEST-SKU", quantity=20)
-    order_line1 = OrderLine(order_reference="order-001", sku="TEST-SKU", quantity=2)
-    order_line2 = OrderLine(order_reference="order-002", sku="TEST-SKU", quantity=3)
-    order_line3 = OrderLine(order_reference="order-002", sku="TEST-SKU", quantity=5)
+    batch = Batch(reference="batch-1", sku="TEST-SKU", quantity=20)
+    order_line1 = OrderLine(order_reference="order-1", sku="TEST-SKU", quantity=2)
+    order_line2 = OrderLine(order_reference="order-2", sku="TEST-SKU", quantity=3)
+    order_line3 = OrderLine(order_reference="order-2", sku="TEST-SKU", quantity=5)
 
     batch.allocate(order_line1)
     batch.allocate(order_line2)
